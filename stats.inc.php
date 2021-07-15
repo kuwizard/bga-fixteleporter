ee948f3615dbe197771a15587a23a7f23b22b8bf
kuwizard <kuzwiz@mail.ru>
2021-07-15 22:34:40 +0100

    added my name and email to the code :)

--- stats.inc.php
+++ stats.inc.php
@@ -3,7 +3,7 @@
 /**
  *------
  * BGA framework: © Gregory Isabelli <gisabelli@boardgamearena.com> & Emmanuel Colin <ecolin@boardgamearena.com>
- * fixtheteleporter implementation : © <Your name here> <Your email address here>
+ * Please Fix the Teleporter implementation : © Pavel Kulagin kuzwiz@mail.ru
  *
  * This code has been produced on the BGA studio platform for use on http://boardgamearena.com.
  * See http://en.boardgamearena.com/#!doc/Studio for more information.
